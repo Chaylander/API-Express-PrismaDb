1e6a7e719da8dd6ffbcf478b07397bd545d7f146
RobertoJáuregui <robotrjj@hotmail.com>
2022-05-16 16:46:15 -0500

    Agrego linter al script, y cambios hechos por linter

--- prisma/seed_2.js
+++ prisma/seed_2.js
@@ -1,39 +1,39 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 (async function main() {
-  try {
-    const woopa = await prisma.explorer_2.upsert({
-        where: { name: 'Woopa' },
-        update: {},
-        create: {
-            name: 'Woopa',
-            lang: 'Español',
-            missionComander: 'Carlo',
-            enrollments: 3,
-            hasCertificate: true
-        },
-    });
+    try {
+        const woopa = await prisma.explorer_2.upsert({
+            where: { name: "Woopa" },
+            update: {},
+            create: {
+                name: "Woopa",
+                lang: "Español",
+                missionComander: "Carlo",
+                enrollments: 3,
+                hasCertificate: true
+            },
+        });
 
-    const woopa1 = await prisma.explorer_2.upsert({
-        where: { name: 'Woopa1' },
-        update: {},
-        create: {
-            name: 'Woopa1',
-            lang: 'Ingles',
-            missionComander: 'Fernanda',
-            enrollments: 1,
-            hasCertificate: false
-        },
-    });
+        const woopa1 = await prisma.explorer_2.upsert({
+            where: { name: "Woopa1" },
+            update: {},
+            create: {
+                name: "Woopa1",
+                lang: "Ingles",
+                missionComander: "Fernanda",
+                enrollments: 1,
+                hasCertificate: false
+            },
+        });
 
 
 
-    console.log('Create 3 woopas in the database');
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
+        console.log("Create 3 woopas in the database");
+    } catch(e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
 })();
--- server_2.js
+++ server_2.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
 // Require para usar Prisma
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-  res.json({message: 'alive'});
+app.get("/", (req, res) => {
+    res.json({message: "alive"});
 });
 
 app.listen(port, () => {
-  console.log(`Listening to requests on port ${port}`);
+    console.log(`Listening to requests on port ${port}`);
 });
 
-app.get('/woopas', async (req, res) => {
+app.get("/woopas", async (req, res) => {
     const allExplorers =  await prisma.explorer_2.findMany({});
     res.json(allExplorers);
 });
 
-app.get('/woopas/:id', async (req, res) => {
+app.get("/woopas/:id", async (req, res) => {
     const id = req.params.id;
     const woopa = await prisma.explorer_2.findUnique({where: {id: parseInt(id)}});
     res.json(woopa);
